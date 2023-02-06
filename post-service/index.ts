db7de7a4f829c288a96ce2026f85905be1d0f053
Bhavya Bhut <pbhavy4@gmail.com>
2023-02-06 22:01:43 +0530

    setup : post microservices

--- post-service/index.ts
+++ post-service/index.ts
@@ -1,13 +1,33 @@
 import express from 'express';
 import cors from 'cors';
+import { randomBytes } from 'crypto';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+interface Posts {
+  [key: string]: {
+    id: string;
+    title: string;
+  };
+}
+
+const posts: Posts = {};
+
 app.post('/post', (req, res) => {
-  const body = req.body;
+  const { title } = req.body;
+  const id = randomBytes(4).toString('hex');
+  posts[id] = {
+    id,
+    title,
+  };
+  res.send(posts[id]);
+});
+
+app.get('/post', (req, res) => {
+  res.send(posts);
 });
 
 app.listen(4001, () => {
